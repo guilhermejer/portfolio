02623e386c91aa8c7df39fb8bca90844afd98bed
guilh <guilhherme.jer0@gmail.com>
2025-02-16 04:47:18 -0300

    +Assets +Componentes

---
+++ src/components/Certifications.tsx
@@ -0,0 +1,59 @@
+
+const Certifications = () => {
+  const certifications = [
+    {
+      title: "AZ 900",
+      link:"https://learn.microsoft.com/pt-br/users/guilhermejeronymo-6324/credentials/511087a61308ce63",
+      description: "Microsoft Certified: Conceitos básicos do Azure",
+      imgsrc:"/public/az900badge.svg",
+      tags: ["Azure", "Cloud", "Network"],
+    },
+    {
+      title: "OCI Foundations",
+      link:"https://catalog-education.oracle.com/ords/certview/sharebadge?id=BAEEA48A3C25B730A2AF7BC9A7066D627071C1FDECAA08702B38F8ABB34D25C9",
+      description: "Oracle Cloud Infrastructure 2024 Certified Foundations Associate",
+      imgsrc:"/public/OCIbadge.png",
+      tags: ["OCI", "Cloud", "Network"],
+    },
+  ];
+
+  return (
+    <section id="certifications" className="section-padding">
+      <div className="container mx-auto">
+        <span className="inline-block px-3 py-1 text-sm rounded-full bg-accent text-secondary-foreground mb-4">
+          Certificados
+        </span>
+        <h2 className="text-3xl md:text-4xl font-bold mb-12">Certificados e Credenciais</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 content-center">
+          {certifications.map((certifications, index) => (
+            <div
+              key={index}
+              className="glass-card p-6 rounded-lg hover:translate-y-[-4px] transition-transform duration-300"
+            >
+              <a
+                target="_blank" rel="noopener noreferrer"
+                href={certifications.link}
+              >
+              <div className="h-48 bg-accent rounded-md mb-4"> <img className="w-full h-full bg-accent/20" src={certifications.imgsrc} alt={certifications.title} /></div>
+              </a>
+              <h3 className="text-xl font-semibold mb-2 text-center">{certifications.title}</h3>
+              <p className="text-foreground/80 mb-4">{certifications.description}</p>
+              <div className="flex flex-wrap gap-2">
+                {certifications.tags.map((tag, tagIndex) => (
+                  <span
+                    key={tagIndex}
+                    className="px-3 py-1 text-sm rounded-full bg-accent/50 text-secondary-foreground"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Certifications;
---
+++ src/components/Experience.tsx
@@ -0,0 +1,31 @@
+
+const Experience = () => {
+    return (
+      <section id="experience" className="section-padding bg-accent/30">
+        <div className="container mx-auto">
+          <div className=" mx-auto">
+            <span className="inline-block px-3 py-1 text-sm rounded-full bg-accent text-secondary-foreground mb-4">
+              Experiência
+            </span>
+            <h2 className="text-3xl md:text-4xl font-bold mb-8">
+              Minha jornada na tecnologia
+            </h2>
+            <div className="glass-card p-8 rounded-lg">
+              <p className="text-lg leading-relaxed mb-6">
+              Sou um profissional com mais de 8 anos de experiência na área de tecnologia, atuando principalmente como Analista de Sistemas com foco em sustentação de sistemas corporativos, DevOps e análise de dados. Possuo formação em Análise e Desenvolvimento de Sistemas pela FIAP e MBA em Data Science & Analytics pela USP/ESALQ, o que me proporciona uma base sólida tanto em desenvolvimento quanto em ciência de dados. Minha carreira tem sido marcada pela atuação em grandes empresas, como Vivo (Telefônica Brasil) e TIM Brasil, onde desenvolvi habilidades em gerenciamento de sistemas antifraude, suporte N3, criação de dashboards em Power BI e desenvolvimento de fluxos ETL.
+              </p>
+              <p className="text-lg leading-relaxed mb-6">            
+              Tenho expertise em diversas tecnologias, incluindo bancos de dados como Oracle, Vertica e MSSQL, além de ferramentas de ETL como Informatica PowerCenter e linguagens de programação como Python, Shell/Bash e R. Sou certificado em Azure (AZ-900) e possuo experiência com cloud computing em plataformas como Oracle Cloud Infrastructure (OCI) e Google Cloud Platform (GCP). Minha atuação em ambientes ágeis, utilizando metodologias como SCRUM, me permitiu desenvolver uma abordagem colaborativa e focada em resultados, sempre buscando agregar valor ao cliente final.
+              </p>
+              <p className="text-lg leading-relaxed">
+              Além da experiência corporativa, tenho interesse em projetos pessoais que envolvem a criação e gerenciamento de ambientes cloud para hospedagem de jogos online, onde aplico conhecimentos em configuração de sistemas operacionais Linux, Docker e gerenciamento de custos operacionais. Sou um entusiasta de machine learning e análise de dados, com projetos que incluem a criação de modelos de regressão linear para previsão de tempo de execução de algoritmos. Meu objetivo é continuar evoluindo na área de tecnologia, contribuindo para a eficiência e inovação dos processos empresariais.
+              </p>          
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Experience;
+  
